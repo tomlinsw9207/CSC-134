65e9d16b61a018e30025e761275cf2cc3b676c3a
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-01-15 16:20:40 +0000

    hullo wurld

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+// CSC 134
+// Hello World -- MY ADDITION
+// Lee Tomlinson
+// January 15 2025
+
+#include <iostream>
+  
+using namespace std;
+  
+int main()
+{
+   cout << "Hello CSC134 queens and baddies!" << endl;
+   cout << "" << endl;
+   cout << "Like and subscribe lol" << endl; 
+   cout << "Stole that one! Shout out to TeacherBot!" << endl;
+   cout << "" << endl;
+   cout << "This is all I know how to \"code\"" << endl;
+   cout << "Pretty cute, right?" << endl;
+   return 0;
+}
