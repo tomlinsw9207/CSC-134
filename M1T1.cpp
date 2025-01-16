4d07214c2ed31b8c04ac162323343d78a2cd8c4d
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-01-16 21:32:46 +0000

    all at once cause im baller

--- M1T1.cpp
+++ M1T1.cpp
@@ -3,3 +3,24 @@
 // Lee Tomlinson
 // January 16 2025
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hey what is up my dudes, my name is Lee Tomlinson" << endl;
+    cout << "" << endl;
+    cout << "I'm currently enrolled in the Associate of Arts program," << endl;
+    cout << "but I'm transferring that to UNC Chapel Hill through CStep" << endl;
+    cout << "" << endl;
+    cout << "I like to read comic books and actual books too" << endl;
+    cout << "Sometimes I do other things" << endl;
+    cout << "Born to be a dweeb, forced to be a member of society" << endl;
+    cout << "" << endl;
+    cout << "" << endl; 
+    cout << "That is all." << endl;
+    
+    return 0;
+
+}
