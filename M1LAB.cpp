dc38a2d2b8b4a4049a51eed08647109b0961d2f9
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-01-27 15:34:59 +0000

    prelim information

---
+++ M1LAB.cpp
@@ -0,0 +1,28 @@
+// CSC 134
+// M1LAB
+// Lee Tomlinson
+// January 27 2025
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // this program will simulate an apple orchard
+    // the owner's name -- Lee Tomlinson
+    string name = "Lee Tomlinson"
+    // number of apples owned -- 100
+    int apples = 100
+    // price per apples -- 0.25
+    double PricePerApple = 0.25
+    // calculate the total price of apples
+    // TODO
+
+    // print all the information about the orchard
+    // TODO
+    cout << "Welcome to " << name;
+    cout << " 's apple orchard!" << endl;
+
+
+return 0;
+}
