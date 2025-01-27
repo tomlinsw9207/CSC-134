24844f9c9b38a597c1af25ff68cedf2d3b7824ae
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-01-27 16:38:07 +0000

    extra edits and stopping place

--- Module 1/M1HW1_tomlinson.cpp
+++ Module 1/M1HW1_tomlinson.cpp
@@ -8,13 +8,13 @@ using namespace std;
 
 int main()
 {
-    string name = "Over The Hedge";
+    string name = "\"Over The Hedge\"";
     string year = "2006";
     double priceGross = 339.8;
     //add boxoffice amount $339.8 million
     //TODO
     cout << "The movie " << name;
-    cout << " was released in the " << year;
+    cout << " was released in the year " << year;
     cout << " and has made a total of $" << priceGross;
     cout << " million in the box office." << endl;
 return 0;
