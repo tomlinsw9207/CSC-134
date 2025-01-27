8c46a7518d29f52f394f06461a44be0f9fdb1d49
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-01-27 16:23:09 +0000

    playing with m1hw1

---
+++ Module 1/M1HW1_tomlinson.cpp
@@ -0,0 +1,17 @@
+// CSC 134
+// M1HW1
+// Lee Tomlinson
+// January 27 2025
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name = "Over The Hedge";
+    string year = "2006";
+
+    //add boxoffice amount $339.8 million
+    //TODO
+
+}
