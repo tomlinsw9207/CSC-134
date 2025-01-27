927790694044b89f1320c253a4fae98213759320
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-01-27 16:33:55 +0000

    check inition strings and couts

--- Module 1/M1HW1_tomlinson.cpp
+++ Module 1/M1HW1_tomlinson.cpp
@@ -10,8 +10,12 @@ int main()
 {
     string name = "Over The Hedge";
     string year = "2006";
-
+    double priceGross = 339.8;
     //add boxoffice amount $339.8 million
     //TODO
-
+    cout << "The movie " << name;
+    cout << " was released in the " << year;
+    cout << " and has made a total of " << priceGross;
+    cout << " million in the box office." << endl;
+return 0;
 }
