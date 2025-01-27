dd65d07a131f541d4d14301384ba226a6d53467d
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-01-27 20:52:18 +0000

    character and quote

--- Module 1/M1HW1_tomlinson.cpp
+++ Module 1/M1HW1_tomlinson.cpp
@@ -11,16 +11,29 @@ int main()
     string name = "\"Over The Hedge\"";
     string year = "2006";
     double priceGross = 339.8;
-    //add boxoffice amount $339.8 million
-    //TODO
+ // intro and summary
     cout << "The movie " << name;
-    cout << " was released in the year " << year;
-    cout << " and has made a total of $" << priceGross;
+    cout << " was released in the year " << year << endl;
+    cout << "and has made a total of $" << priceGross;
     cout << " million in the box office." << endl;
+    cout << "" << endl;
     cout << name;
     cout << " is about a racoon named RJ, who has a large sum of debt." << endl;
     cout << "RJ convinces a bunch of critters that live beside a human suburb to" << endl;
-    cout << "join him in numerous heists and scams, to gather food before they hibernate." << endl;
+    cout << "join him in numerous heists and scams to gather food before they hibernate." << endl;
     cout << "Little did they know that RJ had ulterior motives..." << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+// favorite character Hammy the Squirrel
+    cout << "My fave characters from " << name << endl;
+    cout << "" << endl;
+    cout << "  ~ Hammy the Squirrel" << endl;
+    cout << "  ~ Steve (the hedge)" << endl;
+    cout << "  ~ Stella the Skunk" << endl;
+
+ // favorite quotes
+    cout << "My personal fave quotes from " << name << endl;
+    cout << "" << endl;
+    cout << "\"I can't be arrested! I'm the President of the Homeowners of Association!\" - Gladys (a homeowner/HOA President)" << endl;
 return 0;
 }
