7e46c82b32194d6f5d88f1f545267e3a6d6de029
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-02-03 16:25:20 +0000

    m2t2 finished yippppeeeeeeeeeeee

--- Module 2/M2T2_tomlinson.cpp
+++ Module 2/M2T2_tomlinson.cpp
@@ -17,14 +17,40 @@ int main() {
 // Set up the varibles
     double foodprice = 5.99;  // can create input for this later
     string storename = "Meow Palace";
+    string fooditem = "Meowkshake";
 
 // Do the order
     cout << "Welcome to " << storename << endl;
-    cout << "You have one Meowkshake, that is $5.99" << endl;
+    cout << "You have one " << fooditem << ", that is $" << foodprice << endl;
+    cout << endl;
+    cout << endl;
 
 // Figure out subtotal and total
+    double subtotal, total;
+    subtotal = foodprice; // this changes if they buy more
+// calculate tax
+    double tax = 0.08;
+    double taxowed = subtotal * tax; // tax
+    total = subtotal + taxowed; // total price
+
+    cout << fixed << setprecision (2); // ensures that there are only two decimal places instead of a million that we don't need or care about
+
+// \t is the tab key, so it places a space without needing to add individual spaces to the code
+    cout << "    RECEIPT" << endl;
+    cout << "   ---------" << endl;
+    cout << endl;
+    cout << "1  " << fooditem << endl;
+    cout << endl;
+    cout << "-----------------" << endl;
+    cout << "Subtotal    $" << subtotal << endl;
+    cout << "Tax         $" << taxowed << endl;
+    cout << "-----------------" << endl;
+    cout << "Total       $" << total << endl;
+    cout << "-----------------" << endl;
+    cout << endl;
 
 // Print the receipt
 cout << "Thank you! Have a nice day!" << endl;
+cout << endl;
 return 0;
 }
