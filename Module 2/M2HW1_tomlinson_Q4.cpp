ef46f55eefdb2989a76657f6ec1f9de179fcf168
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-02-10 21:06:36 +0000

    M2HWQ4

---
+++ Module 2/M2HW1_tomlinson_Q4.cpp
@@ -0,0 +1,27 @@
+// CSC 134
+// M2HW1 -- Question 4
+// Wendy "Lee" Tomlinson
+// February 10 2025
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Question 4" << endl; // cheering program
+    cout << endl;
+
+// strings
+    string LetsGo = "Let's go";
+    string school = "FTCC";
+    string team = "Trojans";
+    string cheerOne = LetsGo + " " + school;
+    string cheerTwo = LetsGo + " " + team;
+
+// output
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerTwo << endl;
+return 0;
+}
