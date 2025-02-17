2338cafd34cda3c457736a10b756025fef5a6c35
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-02-17 16:16:23 +0000

    added else and if statements

--- Module 3/m3t1_tomlinson.cpp
+++ Module 3/m3t1_tomlinson.cpp
@@ -9,20 +9,20 @@
 #include <iostream>
 using namespace std;
 
-int main() 
-{
+int main() {
+   
     //variables
     double len1, wid1, len2, wid2;
     double area1, area2;
 
     // ask kthe user for the length and width
-    cout << "What's the length of the first rectangle (inches)? ";
+    cout << "What's the length of the first rectangle (in inches)? ";
     cin >> len1;
-    cout << "What's the width of the first rectangle (inches)? ";
+    cout << "What's the width of the first rectangle (in inches)? ";
     cin >> wid1;
-    cout << "What's the length of the second rectangle (inches) ? ";
+    cout << "What's the length of the second rectangle (in inches)? ";
     cin >> len2;
-    cout << "What's the width of the second rectangle (inches)? ";
+    cout << "What's the width of the second rectangle (in inches)? ";
     cin >> wid2;
     cout << endl;
 
@@ -33,10 +33,17 @@ int main()
     // print the area
     cout << "The area of the first rectangle is " << area1 << " square inches." << endl;
     cout << "The area of the second rectangle is " << area2 << " square inches." << endl;
+    cout << endl;
 
-    // whish is larger?
+    // which is larger?
     if (area1 > area2) {
-        cout << "the first one is larger." << endl;
+        cout << "The first one is larger." << endl;
+    }
+    else if (area2 > area1) {
+        cout << "The second one is larger." << endl;
+    }
+    else if (area2 = area1) {
+        cout << "The rectangles are the same size." << endl;
     }
 
     return 0;
