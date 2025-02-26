ec626ff0d7a366a437faba87e9a7195982521b33
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-02-26 15:27:54 +0000

    set up

---
+++ Module 3/m3lab2_tomlinson.cpp
@@ -0,0 +1,30 @@
+// CSC 134
+// m3lab2 -- grades
+// Wendy "Lee" Tomlinson
+// Feb 26 2025
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // define variables
+    int num_grade;      // 0-100
+    string letter_grade;        // "A", "B", "C"
+
+    // ask for number grade
+    cout << "Number grade to letter grade conversion program!" << endl;
+    cout << "Enter the number grade: ";
+    cin >> num_grade;
+    cout << endl;
+
+    // find the letter grade
+
+
+    // tell user the answer
+    cout << "Your grade of " << num_grade << " is a " << letter_grade << endl;
+
+
+
+    return 0;
+}
