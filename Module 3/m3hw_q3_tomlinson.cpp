d56ae0a0a5f3cb0fdd693a90aa88f16004fbc212
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-02-28 16:31:02 +0000

    m3hw q3 set up

---
+++ Module 3/m3hw_q3_tomlinson.cpp
@@ -0,0 +1,25 @@
+// CSC 134
+// m3hw -- QUESTION 3
+// Wendy "Lee" Tomlinson
+// Feb 28 2025
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "QUESTION 3" << endl;
+
+    // intro statement
+    cout << "Choose your own adventure and see what happens!" << endl;
+    cout << endl;
+
+    // variables
+    int start;      // 1 or 2
+    int choice;     // 1 or 2
+
+    // set up variables for each path option response..? if choice 2 string option2 blah blah, if choice 1 string option1 blah blah?
+
+    return 0;
+}
