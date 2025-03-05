105dfbcc886032befb7d099bf36d6cc1622aac32
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-03-05 15:58:54 +0000

    while loop for calling programs works

--- Module 4/m4t1_tomlinson.cpp
+++ Module 4/m4t1_tomlinson.cpp
@@ -1,5 +1,5 @@
 // CSC 134
-// M4T1
+// M4T1 -- WHILE LOOPS
 // Wendy "Lee" Tomlinson
 // March 5 2025
 
@@ -14,14 +14,57 @@ void program2();    // Gaddis program 5-6
 // main function
 int main() {
 
+
+    // variables
+    int choice;
+    bool keep_going = true;
+
+    // while loop
+    while (true == keep_going) {
+
+        cout << endl;
+        cout << "M4T1 part 1 and part 2" << endl;
+        cout << "1. Gaddis program 5-3 (part 1)" << endl;
+        cout << "2. Gaddis program 5-6 (part 2)" << endl;
+        cout << "3. Quit ";
+        
+        cin >> choice;
+        if (choice == 1) {
+            cout << endl;
+            program1();
+        }
+        else if (choice == 2) {
+            cout << endl;
+            program2();
+        }
+        else if (choice == 3) {
+            cout << endl;
+            cout << "Caio, I guess." << endl;
+            keep_going = false; // immediate exit
+        }
+        else {
+            cout << endl;
+            cout << "Not a valid option";
+        }
+    }
+   
     return 0;
 }
 
-// function definitions
+// demonstrates simple while loop (Gaddis 5-3)
 void program1() {
+    
+    int number = 0;
+
+    while (number <= 7) {
+        cout << "Howdy!" << endl;
+        number++;
+    }
+    cout << "That's all, Partner!" << endl;
 
 }
 
+// loop control variable (Gaddis 5-6)
 void program2() {
-
+    cout << "Program goes here";
 }
