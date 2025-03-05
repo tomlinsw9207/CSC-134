11b918727bb0926458a932bb4aa2ea24787a7b35
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-03-05 15:28:07 +0000

    set up function declaration

---
+++ Module 4/m4t1_tomlinson.cpp
@@ -0,0 +1,27 @@
+// CSC 134
+// M4T1
+// Wendy "Lee" Tomlinson
+// March 5 2025
+
+
+#include <iostream>
+using namespace std;
+
+// function declarations
+void program1();    // Gaddis program 5-3
+void program2();    // Gaddis program 5-6
+
+// main function
+int main() {
+
+    return 0;
+}
+
+// function definitions
+void program1() {
+
+}
+
+void program2() {
+
+}
