8cc3b1f7a72d5f28f3a605368667bb99544eb080
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-03-17 15:10:17 +0000

    work lol

--- Module 4/m4lab1_tomlinson.cpp
+++ Module 4/m4lab1_tomlinson.cpp
@@ -12,10 +12,20 @@ int main() {
     string pixel = "🥺";   // use any character but Unicode characters have to be strings
     // cout << pixel << endl;
 
+    int WIDTH;
+    int HEIGHT;
+    
+    cout << "Enter width: ";
+    cin >> WIDTH;
+    cout << endl;
+    cout << "Enter height: ";
+    cin >> HEIGHT; 
+    cout << endl;
+
     // three ways ot run a loop
     // 1. while loop
     int count = 0;
-    while (count < 10) {
+    while (count < 5) {
         cout << pixel;
         count++;
     }
@@ -27,6 +37,33 @@ int main() {
         cout << pixel << " ";
         count++;
     } while (count < 5);
+    cout << endl;
+
+    // 3. for loop
+    // one time that using i as a variable is okay
+    // i standsd for index
+    cout << endl;
+    cout << pixel << " printed horizontally (row)" << endl;
+    for (int i = 0; i < WIDTH; i++) {
+        cout << pixel << " ";
+    }
+    cout << endl;
+    cout << pixel << " printed verticaly (column)" << endl;
+    for (int i = 0; i < HEIGHT; i++) {
+        cout << pixel << endl;
+    }
+    cout << endl << "Printing a box " << WIDTH << " by " << HEIGHT << endl;
+
+    // this is a nested loop
+    // for each row
+    for (int h = 0; h < HEIGHT; h++) {
+        // print the current row
+        for (int w = 0; w < WIDTH; w++) {
+            cout << pixel << " ";
+        }
+        // go to the next line
+        cout << endl;
+    }
 
 
     return 0;
