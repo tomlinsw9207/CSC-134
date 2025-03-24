3064fc91418ae963cd5a7fec2b475d624d561391
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-03-24 15:36:44 +0000

    part 3 playing

--- Module 4/M4HW1_tomlinson.cpp
+++ Module 4/M4HW1_tomlinson.cpp
@@ -14,19 +14,36 @@ using namespace std;
 int main() {
 
     int selection; 
+    bool keep_going = true;
 
-    cout << "Select question part:" << endl;
-    cout << "1. part 1, 2. part 2, 3. part 3" << endl;
-    cout << "Type 1 2 or 3: ";
-    cin >> selection;
+    while (true == keep_going) {
 
-    if (selection == 1) {
-        part1();
+        cout << "Select question part:" << endl;
+        cout << "1. part 1, 2. part 2, 3. part 3, 0. to quit" << endl;
+        cout << "Type 1, 2, 3, or 0: ";
+        cin >> selection;
+
+        if (selection == 1) {
+            part1();
+            cout << endl;
+        }
+        else if (selection == 2) {
+            part2();
+            cout << endl;
+        }
+        else if (selection == 3){
+            part3();
+            cout << endl;
+        }
+        else if (selection == 0) {
+            cout << "See ya!" << endl;
+            keep_going = false;
+            cout << endl;
+        }
+        else {
+            cout << "Not a valid option" << endl;
+        }
     }
-    else if (selection == 2) {
-        part2();
-    }
-
 
     return 0;
 }
@@ -56,14 +73,40 @@ void part2() {
     cin >> table;
     cout << endl;
 
+    while (n < 12 && table >= 1 && table <= 12) {
+            n++;
+            product = table * n;
+            cout << table << " times " << n << " is " << product << endl;
+    }
+
+}
+
+void part3() {
+
+    int table; 
+    int n = 0;
+    int product;
+    bool okay = false;
+    // input validation
+    while (okay == false) {
+
+    
+    cout << "Input a number from 1 to 12: ";
+    cin >> table;
+    cout << endl;
+
+        if (table >= 1 && table <= 12) {
+            okay = true;
+        }
+    }
 
     while (n < 12 && table >= 1 && table <= 12) {
             n++;
             product = table * n;
             cout << table << " times " << n << " is " << product << endl;
     }
-    
+
+
 
 }
-
 
