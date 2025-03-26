1f2d13ce016bad729f0770f74dac5a9c936918c7
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-03-26 14:28:11 +0000

    value returning function working

--- Module 5/m5t1_tomlinson.cpp
+++ Module 5/m5t1_tomlinson.cpp
@@ -11,6 +11,7 @@ using namespace std;
 // declare void functions
 void first_message();
 void greet_user(string name);
+int double_a_number(int number);
 
 int main() {
 
@@ -18,6 +19,13 @@ int main() {
     first_message();        // call function
     string user = "Mike";
     greet_user(user);       // passing the value "mike" not the actual variable of user/name
+    cout << "back in main(), the name is " << user << endl;
+    int num;
+    cout << "Enter and integer: ";
+    cin >> num;
+    int answer = double_a_number(num);
+    cout << "Double that is " << answer << endl;
+
 
 return 0;
 }
@@ -28,5 +36,10 @@ void first_message() {
 }
 
 void greet_user(string name) {
-    cout  << "Hello, " << name << endl;
+    cout  << "Hello, " << name << " from greet_user(string name)! " << endl;
+}
+
+int double_a_number(int number) {
+    int answer = number * 2;
+    return answer;  // must return an int
 }
