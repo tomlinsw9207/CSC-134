4d88cc96a8801354d8482d68d2cc2de8e0686fe3
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-04-07 15:38:51 +0000

    m5hw q3 done but ugly

--- Module 5/m5hw_tomlinson.cpp
+++ Module 5/m5hw_tomlinson.cpp
@@ -82,6 +82,7 @@ void part1() {
     cout << "The average rainfall for " << month1 << ", " << month2 << ", and " << month3 << " is " << avgrainfall;
 }
 
+// FUNCTION FOR Q2
 void part2() {
     double length, width, height;
     double volume;
@@ -111,6 +112,68 @@ void part2() {
 
     }
 }
-void part3() {}
+
+// FUNCTION FOR Q3 ugly :((
+void part3() {
+    // ask user for an int 1-10
+    // display roman numeral version
+    int num;
+    bool runprogram3 = true;
+    cout << "Enter any integer 1 - 10: ";
+    cin >> num;
+    while (runprogram3 == true) {
+
+        if (num >= 1 && num <= 10) {
+
+            if (num == 1) {
+                cout << "The roman numeral for " << num << " is I" << endl;
+                runprogram3 = false;
+            }
+            else if (num == 2) {
+                cout << "The roman numeral for " << num << " is II" << endl;
+                runprogram3 = false;
+            }
+            else if (num == 3) {
+                cout << "The roman numeral for " << num << " is III" << endl;
+                runprogram3 = false;
+            }
+            else if (num == 4) {
+                cout << "The roman numeral for " << num << " is IV" << endl;
+                runprogram3 = false;
+            }
+            else if (num == 5) {
+                cout << "The roman numeral for " << num << " is V" << endl;
+                runprogram3 = false;
+            }
+            else if (num == 6) {
+                cout << "The roman numeral for " << num << " is VI" << endl;
+                runprogram3 = false;
+            }
+            else if (num == 7) {
+                cout << "The roman numeral for " << num << " is VII" << endl;
+                runprogram3 = false;
+            }
+            else if (num == 8) {
+                cout << "The roman numeral for " << num << " is VIII" << endl;
+                runprogram3 = false;
+            }
+            else if (num == 9) {
+                cout << "The roman numeral for " << num << " is IX" << endl;
+                runprogram3 = false;
+            }
+            else if (num == 10) {
+                cout << "The roman numeral for " << num << " is X" << endl;
+                runprogram3 = false;
+            }
+        }
+        else {
+            cout << "That is not a valid option." << endl;
+            cout << endl;
+            return part3();
+        }
+    }   
+}
+
+
 void part4() {}
 void part5() {}
