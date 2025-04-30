fab2dd1c99639393e0c956cdba261b7c3121c234
tomlinsw9207 <tomlinsw9207@student.faytechcc.edu>
2025-04-30 14:58:46 +0000

    m7t2 gold tier done

--- Module 7/m7t2_tomlinson.cpp
+++ Module 7/m7t2_tomlinson.cpp
@@ -4,44 +4,8 @@
 // 30 april 2025
 
 #include <iostream>
+#include "rectangle.h" // use "" not <> when its ur own file
 using namespace std;
-
-
-class Rectangle {
-    private: 
-        double width;
-        double length;
-
-    public:
-        void    setWidth(double w);
-        void    setLength(double len);
-        // get() should never change anything. const makes sure the data isnt accidentally changed
-        double  getWidth() const;
-        double  getLength() const;
-        double  getArea() const;
-};
-
-// implementation of class  functions
-void Rectangle::setWidth(double w) {
-    width = w;
-}
-
-void Rectangle::setLength(double len) {
-    length = len;
-}
-
-double Rectangle::getWidth() const {
-    return width;
-}
-
-double Rectangle::getLength() const {
-    return length;
-}
-
-double Rectangle::getArea() const {
-    double area = width * length;
-    return area;
-}
 
 
 int main() {
---
+++ Module 7/rectangle.h
@@ -0,0 +1,47 @@
+// csc 134
+// m7 t2
+// Wendy "Lee" Tomlinson
+// these and the last safety thing so that the compiler doesnt include this file more than ONCE
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+
+
+class Rectangle {
+    private: 
+        double width;
+        double length;
+
+    public:
+        void    setWidth(double w);
+        void    setLength(double len);
+        // get() should never change anything. const makes sure the data isnt accidentally changed
+        double  getWidth() const;
+        double  getLength() const;
+        double  getArea() const;
+};
+
+// implementation of class  functions
+void Rectangle::setWidth(double w) {
+    width = w;
+}
+
+void Rectangle::setLength(double len) {
+    length = len;
+}
+
+double Rectangle::getWidth() const {
+    return width;
+}
+
+double Rectangle::getLength() const {
+    return length;
+}
+
+double Rectangle::getArea() const {
+    double area = width * length;
+    return area;
+}
+
+// end of file
+#endif
